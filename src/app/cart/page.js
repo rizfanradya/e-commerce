b31b5bf8cc8a6f3d5c9d0f11671144c81ee5cf51
rizfanradya <rizfankusuma@gmail.com>
2023-09-03 17:59:53 +0700

    up

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -1,4 +1,4 @@
-import CartProduct from "@/app/elements/cartProduct";
+import Link from "next/link";
 
 export default function CartPage() {
   return (
@@ -12,5 +12,25 @@ export default function CartPage() {
         <CartProduct />
       </div>
     </>
+  );
+}
+
+function CartProduct() {
+  return (
+    <Link href={"/"} className="w-full p-2 bg-slate-800 rounded-lg flex">
+      <div className="w-16 h-16 bg-slate-700 mr-3"></div>
+      <div className="flex justify-between w-full">
+        <div className="flex flex-col justify-between">
+          <h1 className="font-semibold text-slate-300 text-sm">Nama Barang</h1>
+          <p className="font-medium text-sm">Rp 1.350.000</p>
+        </div>
+        <div className="flex gap-7 items-end">
+          <div className="text-xs">Qty: 1</div>
+          <button className="text-red-400 p-[6px] h-min text-sm rounded-full border border-red-400 flex items-end hover:text-slate-800 hover:bg-red-400 transition">
+            Delete
+          </button>
+        </div>
+      </div>
+    </Link>
   );
 }
--- src/app/category/page.js
+++ src/app/category/page.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Link from "next/link";
 
 export default async function CategoryPage() {
   const getDataProductCategories = await axios.get(
@@ -9,7 +10,17 @@ export default async function CategoryPage() {
   return (
     <>
       <title>Category</title>
-      <div className="my-12">Coming Soon</div>
+      <div className="my-14 w-11/12 m-auto flex flex-wrap gap-6 justify-around">
+        {categories.map((category) => (
+          <Link
+            href={"/"}
+            className="outline outline-1 outline-indigo-500 outline-offset-2 font-semibold tracking-wide rounded-lg p-3 hover:bg-indigo-500 hover:text-slate-800 transition"
+            key={[category]}
+          >
+            {[category]}
+          </Link>
+        ))}
+      </div>
     </>
   );
 }
