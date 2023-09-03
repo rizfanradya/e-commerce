7f317bc2bda1d8d8ac69eacbae7b1d105753c5bd
rizfanradya <rizfankusuma@gmail.com>
2023-09-03 10:21:27 +0700

    up

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,12 +1,22 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function ProfilePage() {
+  const sizeImage = 200;
+
   return (
     <>
       <title>Profile</title>
       <div className="my-12 w-11/12 m-auto flex flex-col items-center gap-4">
         <div className="w-full h-40 rounded-xl bg-gradient-to-r from-violet-500 to-fuchsia-500 p-5 flex items-center gap-4">
-          <div className="bg-slate-400 w-20 h-20 rounded-full"></div>
+          <div className="bg-slate-400 w-20 h-20 rounded-full overflow-hidden">
+            <Image
+              src={"/profile.png"}
+              alt="profile"
+              width={sizeImage}
+              height={sizeImage}
+            />
+          </div>
           <div className="flex flex-col gap-1">
             <h1 className="font-bold text-sm tracking-wide">Rizfan Radya</h1>
             <p className="text-xs">Lorem Ipsum Dolor Sit Amet.</p>
