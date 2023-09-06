40e43056bc100ee37c85a7d3801769a5305db34e
rizfanradya <rizfankusuma@gmail.com>
2023-09-06 21:35:31 +0700

    up

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -11,28 +11,5 @@ const firebaseConfig = {
   measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyANsv46AEQIgyUAIBXzOox2hVcwLU_1zrg",
-//   authDomain: "rizfan-f8e9f.firebaseapp.com",
-//   databaseURL:
-//     "https://rizfan-f8e9f-default-rtdb.asia-southeast1.firebasedatabase.app",
-//   projectId: "rizfan-f8e9f",
-//   storageBucket: "rizfan-f8e9f.appspot.com",
-//   messagingSenderId: "999866717138",
-//   appId: "1:999866717138:web:45b42390ba5bd93b332e0b",
-//   measurementId: "G-8NW6FSVK72",
-// };
-
 const firebaseApp = initializeApp(firebaseConfig);
 export default firebaseApp;
-
-// try {
-//   const docRef = await addDoc(collection(db, collectionName), {
-//     nama: "radya",
-//     email: "rizfanradya@gmail.com",
-//     nohp: 88747482005,
-//   });
-//   console.log("Document written with ID: ", docRef.id);
-// } catch (e) {
-//   console.error("Error adding document: ", e);
-// }
